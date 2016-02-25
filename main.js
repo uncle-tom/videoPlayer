6eeae2b3e8f2e966fd9c79be027739ebc8a428be
Vladimir Samojlov <v-moi@ya.ru>
2016-02-26 01:01:04 +0200

    third

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
 var advStop = 0;
+
+var advClose = function(){
+  $('.adv').fadeOut();
+  $('.adv--close').fadeOut();
+}
+
 function showAdv(){
-  var advCount = 0;
   $('.adv--close').on("click", function(){
-        $('.adv').fadeOut();
-        advStop = 1;
-        return advStop;
-      })
+    advClose();
+    advStop = 1;
+  })
+  var advCount = 0;
   if (advCount == 0) {
     $('.adv').fadeIn(900);
+    $('.adv--close').fadeIn(900);
     $.getJSON('http://constitution.com.ua/ads.json', function(resp){ 
       var HowManyAdv = resp.Adv.length;
       var i = Math.floor(Math.random()*HowManyAdv);
       var durationAdv = resp.Adv[i].duration;
-      $('.adv__title').html(resp.Adv[i].title);
-      $('.adv__description').html(resp.Adv[i].description);
-      
+      $('.adv__main--title').html(resp.Adv[i].title);
+      $('.adv__main--description').html(resp.Adv[i].description);
       $('.adv').on("click", function(){
         $('.adv').fadeOut();
         window.open(resp.Adv[i].href);
         advStop = 1;
       })
-      console.log(advStop);
-        setTimeout(DurationAdv, durationAdv);
+      setTimeout(DurationAdv, durationAdv);
     })
     advCount = 1;
   } 
@@ -29,8 +33,7 @@ function showAdv(){
 
 function DurationAdv(){
   if(advStop == 0) {
-    $('.adv').fadeOut();
-    console.log("Запущено")
+    advClose();
     setTimeout(showAdv,800)  
   }
 }
