5a4feead3c09271a1a73453d41796ba686bce048
Vladimir Samojlov <v-moi@ya.ru>
2016-02-25 14:55:22 +0200

    second

--- main.js
+++ main.js
@@ -6,8 +6,13 @@ function showAdv(){
     var i = Math.floor(Math.random()*HowManyAdv);
     $.getJSON('http://constitution.com.ua/ads.json', function(resp){ 
       $('.adv__title').html(resp.Adv[i].title);
+      $('.adv__description').html(resp.Adv[i].description);
+      $('.adv').on("click", function(){
+        $('.adv').fadeOut();
+        window.open(resp.Adv[i].href);
+        setTimeout(showAdv,4000)
+  })
     })
-    //$('.adv__title').html(json.Adv[i].title);
     advCount = 1;
   } 
 }
@@ -18,16 +23,3 @@ $('.adv__close').on("click", function(){
   $('.adv').fadeOut();
   setTimeout(showAdv,4000)
 })
-
-var json = {
-  "Adv":[{
-      "title":"Yandex.ru",
-      "description":"Description First Post",
-      "href": "http://www.ya.ru"
-      },
-    {
-      "title":"Google.com",
-      "description":"Description Second Post",
-      "href": "http://www.google.com"
-      }]
-};
